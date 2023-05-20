77aa5c28ad42cd184b7b29cd5cc02b34824651c7
kyb <kybdev@gmail.com>
2023-05-20 21:58:14 +0900

    <:debug> tailwind 적응 중

--- src/components/post/writer.tsx
+++ src/components/post/writer.tsx
@@ -3,25 +3,33 @@
 import React from "react";
 import ToastEditor from "@/components/toast/editor";
 
-type Props = {}
 
+type Props = {}
 
 export default function PostWriter(props: Props) {
     const ref = React.useRef<any>(null);
 
     return (
-        <div className={'w-screen flex flex-col pt-16 pl-36 pr-36'}>
-            <div className={'flex mb-4 w-full'}>
-                <button className={'block w-full bg-indigo-600 text-sky-400'}>
-                    작성 하기 1
-                </button>
+        <div className={root}>
+            <div className={item}>
+                <button className={submit}> 글 쓰기</button>
             </div>
 
-            <div className={'flex mb-4 w-full'}>
+            <div className={item}>
+                <input type="text" className={title} placeholder="제목 입력 해주세요"/>
+            </div>
+
+            <div className={[item, 'h-screen'].join(' ')}>
                 <ToastEditor editorRef={ref}/>
             </div>
         </div>
 
     )
 }
+
+const root: string = 'w-screen flex flex-col pt-16 pl-36 pr-36'
+const item: string = 'flex mb-4 w-full'
+const submit: string = 'block shadow-lg rounded w-32 h-10 bg-indigo-600 text-amber-50'
+const title: string = 'rounded w-full h-12 border-2 p-4'
+
 
